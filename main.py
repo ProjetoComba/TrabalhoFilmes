67716d6bb4f0b8fe811e3273dbc841362f047093
ProjetoComba <lufedinesh@gmail.com>
2021-04-20 14:05:30 -0300

    main

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
 import csv
 
+def media_count_rating(HashTableMediaCount, M):
+    with open('rating.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        csv_reader.__next__()
+
+        for row in csv_reader:
+            movieID = int(row[1])
+            rating = float(row[2])
+
+            HashTableMediaCount[movieID][0] += rating
+            HashTableMediaCount[movieID][1] += 1
+
+        for i in range(M):
+            if (HashTableMediaCount[i][0] != 0):
+                HashTableMediaCount[i][0] = round(HashTableMediaCount[i][0] / HashTableMediaCount[i][1], 6)
+
+        return HashTableMediaCount
+
+
+
+def movies_dados(HashTableMovies):
+##### salvando os dados dos filmes em uma tabela hash ######
+    with open('movie_clean.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        csv_reader.__next__()
+
+        for row in csv_reader:
+            movieID = int(row[0])
+            nome = str(row[1])
+            genero = str(row[2])
+
+            HashTableMovies[movieID][0] = nome
+            HashTableMovies[movieID][1] = genero
+
+        return HashTableMovies
+
+
+######## main #########
 M = 131263
 HashTableMediaCount = [[0.0, 0] for _ in range(M)]  # INICIALIZANDO A TABELA
 
-##### calculando a média e número de ratings ######
-with open('rating.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+HashTableMediaCount = media_count_rating(HashTableMediaCount, M)
 
-    csv_reader.__next__()
-
-    for row in csv_reader:
-        movieID = int(row[1])
-        rating = float(row[2])
-
-        HashTableMediaCount[movieID][0] += rating
-        HashTableMediaCount[movieID][1] += 1
-
-    for i in range(M):
-        if(HashTableMediaCount[i][0] != 0):
-            HashTableMediaCount[i][0] = round(HashTableMediaCount[i][0]/HashTableMediaCount[i][1], 6)
-
-
-# for i in range(1, M):
-#     if (HashTableMediaCount[i][0] != 0):
-#         print(i, HashTableMediaCount[i])
+for i in range(1, M):
+    if (HashTableMediaCount[i][0] != 0):
+        print(i, HashTableMediaCount[i])
 
 HashTableMovies = [['', ''] for _ in range(M)]  # INICIALIZANDO A TABELA
 
-##### salvando os dados dos filmes em uma tabela hash ######
-with open('movie_clean.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-
-    csv_reader.__next__()
-
-    for row in csv_reader:
-        movieID = int(row[0])
-        nome = str(row[1])
-        genero = str(row[2])
-
-        HashTableMovies[movieID][0] = nome
-        HashTableMovies[movieID][1] = genero
-
-
+HashTableMovies = movies_dados(HashTableMovies)
+#
+#
+# # for i in range(1, M):
+# #     if (HashTableMovies[i][0] != ''):
+# #         print(i, HashTableMovies[i], HashTableMediaCount[i])
+#
+for i in range(1, M):
+    if (HashTableMovies[i][0] != ''):
+        print(f"{i} | {'| '.join(map(str, HashTableMovies[i]))} {'| '.join(map(str, HashTableMediaCount[i]))}")
